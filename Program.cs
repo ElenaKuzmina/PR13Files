494e536803bb8fb12758c9b2b78ac5937f6adccb
Админ <@DESKTOP-RPOBN6D>
2022-03-28 11:11:06 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace PR13Files
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //запись исходных данных в файл Input.txt
+            string path = @"Input.txt";
+            double p = 33.3e2;
+            StreamWriter sw = new StreamWriter(path, false);
+            sw.WriteLine(p);
+            sw.Close();
+            Console.WriteLine("Запись произведена");
+            Console.ReadKey();
+            //чтение из файла Input.txt
+            StreamReader sr = new StreamReader(path);
+            double p_read = Convert.ToDouble(sr.ReadLine());
+            //вычисление результата по формуле
+            double solve = Math.Log(p_read);//формула
+            //запись результата в файл Result.txt
+            string path1 = @"Result.txt";
+            StreamWriter sw1 = new StreamWriter(path1, false);
+            sw1.WriteLine(solve);
+            sw1.Close();
+            Console.WriteLine("Подсчет и Запись выполнены");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("PR13Files")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PR13Files")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("fce0b3d9-d24f-4762-98f9-6ffe2bb4af60")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
